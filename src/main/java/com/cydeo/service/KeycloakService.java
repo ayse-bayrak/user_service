6d6d39e39f9f15268b3fd359a4076be646fdc166
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-09-01 18:46:32 +0300

    created getAccessToken method

---
+++ src/main/java/com/cydeo/service/KeycloakService.java
@@ -0,0 +1,14 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.UserDTO;
+
+public interface KeycloakService {
+
+    String getAccessToken();
+    void userCreate(UserDTO userDTO);
+
+    void userUpdate(UserDTO userDTO);
+
+    void delete(String userName);
+
+}
